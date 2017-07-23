4ba045f9f10b46e2c08b791ccfdf095d3e9f592f
Cas de Rooij <casdrooij@gmail.com>
2017-07-23 17:28:11 +0200

    AboutController test for production 8

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
@@ -11,8 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(ProjectsTableSeeder::class);
-        $this->call(AboutTableSeeder::class);
+
+            Eloquent::unguard();
+
+            $this->call('AboutTableSeeder');
+    }
+
+}
+
+class AboutTableSeeder extends Seeder{
+
+    public function run(){
+
+        DB::table('tipos')->insert(array(
+            'name' => 'Interaction Design',
+            'photo' => 'profiel.jpg',
+            'text' => '["Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.","Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi.","Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus."]',
+            'social' => '[{"socialmedia":"twitter","url":"twitter.com"},{"socialmedia":"facebook","url":"facebook.com"}]',
+            'email' => 'casdrooij@gmail.com'
+        ));
     }
 }
