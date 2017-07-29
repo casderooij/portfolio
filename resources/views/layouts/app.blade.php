fdb31b8e645e626a576e27c2e2d6cd9e4d74712f
Cas de Rooij <casdrooij@gmail.com>
2017-07-29 15:08:26 +0200

    Addition of favicons and redirection of 404 error to the homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,10 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}" sizes="16x16">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}" sizes="32x32">
+    <link rel="icon" type="image/png" href="{{ asset('favicon-96x96.png') }}" sizes="96x96">
 </head>
 <body>
     <div id="app">
