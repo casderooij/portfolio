7fcd5953140d669a616a65c76e55e753fa842fde
Cas de Rooij <hello@casderooij.nl>
2025-09-07 17:14:25 +0200

    Make scroll down its own component

---
+++ src/mediaSphere.ts
@@ -0,0 +1,82 @@
+import type { ProjectItem } from './components/projectsSphere/ProjectsSphereScene'
+
+export const media: ProjectItem[] = [
+  {
+    type: 'video',
+    url: '/project-sphere-media/diffusion.mp4',
+    aspect: 1,
+    title: 'Diffusion algorithm',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/falling-words.mp4',
+    aspect: 1.75,
+    title: 'Falling words',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/green-hills.mp4',
+    aspect: 1,
+    title: 'Green hills',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/grid.mp4',
+    aspect: 1,
+    title: 'Pattern generator',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/idomeneo.mp4',
+    aspect: 1.77,
+    title: 'Idomeneo digital poster',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/marching-squares-lowres.mp4',
+    aspect: 1,
+    title: 'Marching squares lowres',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/marching-squares.mp4',
+    aspect: 1,
+    title: 'Marching squares',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/noise-gallery.mp4',
+    aspect: 1,
+    title: 'Noise gallery',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/number-grid.mp4',
+    aspect: 1,
+    title: 'Number grid',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/organic-chart.mp4',
+    aspect: 1,
+    title: 'Organic chart',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/sliding-ui.mp4',
+    aspect: 1,
+    title: 'Sliding UI',
+  },
+  {
+    type: 'video',
+    url: '/project-sphere-media/window.mp4',
+    aspect: 1,
+    title: 'CSS window',
+  },
+  {
+    type: 'image',
+    url: 'https://placehold.co/10',
+    aspect: 1,
+    title: 'One minute capture',
+  },
+]
